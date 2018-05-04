7a29a0bcf1241f86d9f8b6d84bf035d668edb842
ja14000 <j.awogboro@gmail.com>
2018-05-04 09:01:53 +0100

    Add project files.

---
+++ Tap.Plugins.FieldFoxDemo/FieldFox.cs
@@ -0,0 +1,136 @@
+﻿// Author: MyName
+// Copyright:   Copyright 2018 Keysight Technologies
+//              You have a royalty-free right to use, modify, reproduce and distribute
+//              the sample application files (and/or any modified version) in any way
+//              you find useful, provided that you agree that Keysight Technologies has no
+//              warranty, obligations or liability for any sample application files.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using Keysight.Tap;
+
+//Note this template assumes that you have a SCPI based instrument, and accordingly
+//extends the ScpiInstrument base class.
+
+//If you do NOT have a SCPI based instrument, you should modify this instance to extend
+//the (less powerful) Instrument base class.
+
+namespace Tap.Plugins.FieldFoxDemo
+{
+    [Display("FieldFox", Group: "FieldFoxDemo", Description: "FieldFox N9917A")]
+    [ShortName("FieldFox")]
+    public class FieldFox : ScpiInstrument
+    {
+        #region Settings
+        // ToDo: Add property here for each parameter the end user should be able to change
+        #endregion
+        public FieldFox()
+        {
+            
+          //ScpiCommand("SYSTem:PRESet");
+
+        }
+
+        public override void Open()
+        {
+
+            base.Open();
+
+            if (!IdnString.Contains("N9"))
+            {
+                Log.Error("This instrument driver does not support the connected instrument.");
+                throw new ArgumentException("Wrong instrument type.");
+            }
+
+        }
+
+        public override void Close()
+        {
+            // TODO:  Shut down the connection to the instrument here.
+            base.Close();
+        }
+
+        public void RadioMode(double StationFrequency)
+        {
+            ScpiCommand(@"INSTrument:SELect ""SA"""); //Select the Spectrum Analyser instrument
+            ScpiCommand("SENSe:MEASurement:TAListen FMW"); //Activate FM Wide listening mode
+            ScpiCommand(":SENSe:TAListen:TFReq " + StationFrequency); //Tune to radio station
+            ScpiCommand(":SENSe:POWer:RF:GAIN:STATe 1"); //Turn On the Pre-Amp
+    
+        }
+
+        public void SAView(double CenterFrequency)
+        {
+            ScpiCommand(":SENSe:FREQuency:CENTer " + CenterFrequency); //Set the center frequency to the center frequecy
+            
+            
+        }
+
+        public void ScanStations(double StartFrequency, double StopFrequency)
+        {
+            ScpiCommand(":SENS:FREQ:STAR "+StartFrequency); //Set Start Frequency
+            ScpiCommand(":SENS:FREQ:STOP "+StopFrequency); //Set StopFrequency
+        }
+
+        public double[] GetData()
+        { 
+            ScpiCommand("SWE:POIN 401"); //Set No. of points
+            ScpiCommand("TRAC1:TYPE AVG"); //Set trace type
+            ScpiCommand("DISPlay:WINDow:TRACe:Y:SCALe:AUTO");
+            return ScpiQuery<double[]>("TRAC1:DATA?");
+        }
+
+        public List<double> CalcFrequency(double StartFrequency, double StopFrequency)
+        {
+            var FrequencyStep = ((StopFrequency - StartFrequency) / (401));
+            List<double> FrequencyList = new List<double>();
+            FrequencyList.Add(StartFrequency);
+                   
+            for (int runs = 0; runs < 400; runs++)
+            {
+                FrequencyList.Add(FrequencyList[runs] + FrequencyStep);
+            }
+
+            return FrequencyList;
+        }
+
+        public double[] StationsFound(int AmplitudeCutOff, double[] MeasurementResults) 
+        {
+            var StationsFoundArray = MeasurementResults.Where(item => item >= AmplitudeCutOff).ToArray();
+       
+            return StationsFoundArray;
+        }
+
+        public List<double> FrequenciesFound(int AmplitudeCutOff, double[] MeasurementResults, List<double> FrequencyList)
+        {
+            List<double> StationFrequencyList = new List<double>();
+            var x = 0;
+
+            foreach (double i in MeasurementResults)
+
+            {
+
+                if (MeasurementResults[x] > AmplitudeCutOff)
+                {
+                    
+                    StationFrequencyList.Add(FrequencyList[x]);
+                    x++;
+                }
+
+                else
+                {
+                    x++;
+                }
+            }
+
+            return StationFrequencyList;
+
+
+        }
+    }
+
+    
+}
+
---
+++ Tap.Plugins.FieldFoxDemo/Step.cs
@@ -0,0 +1,87 @@
+﻿// Author: MyName
+// Copyright:   Copyright 2018 Keysight Technologies
+//              You have a royalty-free right to use, modify, reproduce and distribute
+//              the sample application files (and/or any modified version) in any way
+//              you find useful, provided that you agree that Keysight Technologies has no
+//              warranty, obligations or liability for any sample application files.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+using Keysight.Tap;  // Use Platform infrastructure/core components (log,TestStep definition, etc)
+
+namespace Tap.Plugins.FieldFoxDemo
+{
+    [Display("Step", Group: "FieldFoxDemo", Description: "Tune in to a radio station, capture data, plot graphs")]
+    public class Step : TestStep
+    {
+        #region Settings
+        // ToDo: Add property here for each parameter the end user should be able to change.
+        #endregion
+
+        //Creates a UI dropdown for the variable
+        [Unit("Hz", UseEngineeringPrefix: true)]
+        public double StationFrequency { get; set; } 
+        [Unit("Hz", UseEngineeringPrefix: true)]
+        public double CenterFrequency { get; set; }
+        [Unit("Hz", UseEngineeringPrefix: true)]
+        public double StartFrequency { get; set; }
+        [Unit("Hz", UseEngineeringPrefix: true)]
+        public double StopFrequency { get; set; }
+        [Unit("dBm", UseEngineeringPrefix: true)]
+        public int AmplitudeCutOff { get; set; }
+
+
+        // Instrument Declarations (Creates dropdown in TAP GUI))
+        public FieldFox FF { get; set; }
+                
+        public Step()
+        {
+            //Set default values for properties / settings.
+            StationFrequency = 104.1e6;
+            CenterFrequency = StationFrequency;
+            StartFrequency = 80000000;
+            StopFrequency = 108000000;
+            AmplitudeCutOff = -80;
+
+        }
+                
+        public override void PrePlanRun()
+        {
+            base.PrePlanRun();
+            
+        }
+
+        public override void Run()
+        {
+           FF.RadioMode(StationFrequency);
+           FF.SAView(CenterFrequency);
+           FF.ScanStations(StartFrequency, StopFrequency);
+            
+
+           var MeasurementResults = FF.GetData();
+
+
+           var StationsArray = FF.StationsFound(AmplitudeCutOff, MeasurementResults);
+
+           var FrequencyList = FF.CalcFrequency(StartFrequency, StopFrequency);
+           var FrequencyArray = FrequencyList.ToArray(); 
+                                  
+           var FrequenciesFoundList = FF.FrequenciesFound(AmplitudeCutOff, MeasurementResults, FrequencyList);
+           var FrequenciesFoundArray = FrequenciesFoundList.ToArray();
+
+
+           Results.PublishTable("FM Spectrum View", new List<string> {"Frequency", "Amplitude" },FrequencyArray, MeasurementResults);
+           Results.PublishTable("Frequencies Above Cutoff", new List<string> {"Station Frequency(Hz)", "Station Amplitude(dBm)"}, FrequenciesFoundArray, StationsArray);
+
+        }
+    
+
+        public override void PostPlanRun()
+        {
+            base.PostPlanRun();
+        }
+    }
+}
